a39877309813b938537467bbcb0ebac2602af683
Romanellac <101135593+Romanellac@users.noreply.github.com>
2022-06-16 21:05:31 -0300

    Archivo read_txt_files
    Cree el archivo que va a contener las funciones que recorren los archivos de texto

---
+++ read_txt_files.py
@@ -0,0 +1,61 @@
+from notes import notes_mapping
+
+def music_sheet (file):
+    '''
+    Returns a list of lists with the starting time, the frequency and the lasting time of all the notes from
+    the sheet file.
+
+    Parameters
+    ----------
+        file : file
+
+    Returns 
+    -------
+        line_info : list
+            The list in question.
+
+    '''
+    line_info = []
+    with open (file, 'r') as f:
+        for line in f:
+            info = line.split(' ')
+            if info[0].startswith('.'):
+                starts = 0.5
+            else:
+                starts = int(info[0])
+            
+            note = get_frequency(info[1])
+
+            if info[2].startswith('.'):
+                duration = 0.5
+            else:
+                duration = int(info[2])
+
+            line_info.append([starts, note, duration])
+    
+    return line_info
+
+
+def get_frequency (note):
+    '''
+    Returns the frequency of a given note by searching it on a list of tuples.
+
+    Parameters
+    ----------
+    note : str
+        The note in question.
+
+    Returns
+    -------
+        The frequency related to the note.
+    
+    '''
+    for tuple in notes_mapping:
+        if note == tuple[0]:
+            return tuple[0]
+        else:
+            continue
+
+def sorting():
+
+
