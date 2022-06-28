802747dd3935b8371292108ba5baf33617bcb94d
Tiziano Levi Martin Bernal <tmartinbernal@udesa.edu.ar>
2022-06-28 09:26:47 -0300

    Parse

---
+++ Functions/parse.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Execute the python synthesizer")
+parser.add_argument("-f", "--frequency", action="store" ,
+default=48000, type=int, help="Sample rate", choices= [8000, 9600, 11025, 12000,
+16000, 22050, 24000, 32000, 441000, 48000, 88200, 96000])
+parser.add_argument("-i", "--instrument", action="store",
+help="File with armonics and functions for attack, sustain and decay",
+type = str)
+parser.add_argument("-p", "--score",type=str, action="store",
+help="File with the notes and the duration of each one")
+parser.add_argument("-o","--wav" ,action = "store",type=str,
+default="wavfile.wav", help="Name of the wav file to generate")
+args = parser.parse_args()
+frequency = args.frequency
+instrument = args.instrument
+score = args.score
+wavfile = args.wav
--- Functions/read_txt_files.py
+++ Functions/read_txt_files.py
@@ -1,17 +1,14 @@
-from re import L
 from notes import notes_mapping
 
-
-
-def music_sheet (file):
+def music_sheet (file: str) -> list:
     '''
     Returns a list of lists with the starting time, the frequency and the lasting time of all the notes from
     the sheet file.
 
     Parameters
     ----------
-        file : file
-
+        file : str
+            The name of the file.
     Returns 
     -------
         line_info : list
@@ -21,14 +18,11 @@ def music_sheet (file):
     line_info = []
     with open (file, 'r') as f:
         for line in f:
-            line.strip('\n')
             info = line.split(' ')
-            print(info)
-            print('0: ', info[0], '1: ', info[1], '2: ', info[2])
             if info[0].startswith('.'):
                 starts = 0.5
             else:
-                starts = int(info[0])
+                starts = float(info[0])
             
             note = get_frequency(info[1])
 
@@ -36,15 +30,13 @@ def music_sheet (file):
                 duration = 0.5
             else:
                 duration = int(info[2])
-            print('arrived')
-            line_info.append([starts, note, duration])
 
-    
+            line_info.append([starts, note, duration])
     
     return line_info
 
 
-def get_frequency (note):
+def get_frequency (note: str) -> int:
     '''
     Returns the frequency of a given note by searching it on a list of tuples.
 
@@ -63,65 +55,3 @@ def get_frequency (note):
             return tuple[1]
         else:
             continue
-
-def harmonics_info (file):
-    with open(file, 'r') as f:
-        i = 0
-        length = f.readlines()
-        harmonic_info = []
-        A_S_D = []
-        while i < len(length):
-            line = f.readline()
-            if i == 0:
-                i+=1
-            elif i >0 and i < len(length) - 3:
-                harmonic_info.append(line.split(' '))
-    
-            else:
-                A_S_D.append(line.split(' '))
-
-        sorting_harmonics(harmonic_info)
-    
-    print(harmonics_info)
-    return harmonics_info
-
-music_sheet_info =  music_sheet ('example.txt')  
-harmonics_info = (harmonics_info('piano.txt'))
-
-def sorting_harmonics(harmonics):
-    if len(harmonics) > 1:
-        r = len(harmonics) // 2
-        L = harmonics[:r]
-        M = harmonics[r:]
-
-        sorting_harmonics(L)
-        sorting_harmonics(M)
-
-        i = j = k = 0
-
-        while i < len(L) and j < len(M):
-            if L[i][0] < M[i][0]:
-                harmonics[k] = L[i]
-                i+=1
-            else:
-                harmonics[k] = M[j]
-                j+=1
-            k+=1
-
-
-        while i < len(L):
-            harmonics[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(M):
-            harmonics[k] = M[j]
-            j += 1
-            k += 1
-
-    return harmonics
-
-
-
-
-#sort de las notas, algoritmo para añadirlas
